b8e2e3c5c4ca1f1d27b87b4f43dc0c5208df5783
Tshoms <tshomsjordy@gmail.com>
2023-02-07 16:01:14 +0100

    feature/F02: NavbarRightSide > Add span of number in heartlogo and cartlogo

--- src/components/Pages/pages/home/Main.jsx
+++ src/components/Pages/pages/home/Main.jsx
@@ -25,37 +25,6 @@ const MainStyled = styled.div`
   width: 100%;
   /* border: 1px solid red; */
   justify-content: center;
-
-  /* .grid-items {
-    display: flex;
-    flex-direction: column;
-    width: 280px;
-    height: 200px;
-    border-radius: 12px;
-    border: 2px solid black;
-
-    .img-products {
-      height: 70%;
-      width: 100%;
-      border-top-left-radius: 10px;
-      border-top-right-radius: 10px;
-      border: 1px solid black;
-
-      img {
-        width: 100%;
-        height: 100%;
-        border-top-left-radius: 10px;
-        border-top-right-radius: 10px;
-        object-fit: cover;
-
-        /* object-fit: contain; 
-      }
-    }
-
-    .title-products {
-      height: 10%;
-    } 
-  }*/
 `;
 
 export default Main;
